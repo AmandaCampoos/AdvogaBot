f853e8a50d704c03494ea77db63a15e7e6726029
Amanda Campos <amandacamposx2@gamil.com>
2025-04-26 14:49:05 -0400

    ♻️  refactor(script): trocando 'apt-get' por 'yum' para a instalação do docker e clonando o repositório do projeto para ec2

--- scripts/script_inicial_ec2.sh
+++ scripts/script_inicial_ec2.sh
@@ -2,27 +2,26 @@
 set -e
 
 echo "📦 Atualizando pacotes..."
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo yum update -y
 
 echo "🐳 Instalando Docker..."
-sudo apt-get install -y docker.io
+sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
 echo "💻 Instalando Docker Compose..."
-sudo apt-get install -y docker-compose
+sudo yum install -y docker-compose-plugin
 
 echo "🔑 Configurando permissões para o usuário atual..."
 sudo usermod -aG docker $USER
 newgrp docker
 
 echo "📂 Clonando repositório..."
-# colocar o repositorio correto em breve
-git clone -b grupo-2 https://github.com 
-cd SEU_REPOSITORIO/docker
+git clone -b grupo-2 https://github.com/Compass-pb-aws-2025-JANEIRO/sprints-7-8-pb-aws-janeiro.git
+
+cd sprints-7-8-pb-aws-janeiro/docker
 
 echo "🚀 Subindo containers..."
-docker-compose up -d --build
+docker compose up -d --build
 
 echo "✅ Tudo pronto! Bot rodando!"
